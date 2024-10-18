cf3cc6d64e14fed696c93320b84bdedfe8b42382
Minsu Kim <70056622+DevMinsuKim@users.noreply.github.com>
2024-10-18 22:18:16 +0900

    Title: 두 사람, Time: 214ms, Memory: 14MB, Status: Passed - CodeTree

--- 241018/두 사람/two-person.js
+++ 241018/두 사람/two-person.js
@@ -7,7 +7,7 @@ let b = input[1].split(" ")
 let ageA = Number(a[0]), genderA = a[1]
 let ageB = Number(b[0]), genderB = b[1]
 
-if ((ageA >= 19 || ageB >= 19) && (genderA == "M" || genderB == "M")) {
+if ((ageA >= 19 && genderA === "M") || (ageB >= 19 && genderB === "M")) {
     console.log(1)
 } else {
     console.log(0)
