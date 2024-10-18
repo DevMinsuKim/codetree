ecb3b3d8bfc3b2fe437c7945527f7ce6859d2a63
Minsu Kim <70056622+DevMinsuKim@users.noreply.github.com>
2024-10-19 00:10:11 +0900

    Title: a의 배수들의 곱, Time: 287ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 241019/a의 배수들의 곱/product-of-multiples-of-a.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let arr = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(arr[0])
+let b = Number(arr[1])
+
+let prod = 1;
+
+for (let i = 1; i <= b; i++) {
+    if (i % a === 0) {
+        prod *= i
+    }
+}
+
+console.log(prod);
