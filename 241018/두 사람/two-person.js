75139a149f050bbfc601b1efe4393b57024977cb
Minsu Kim <70056622+DevMinsuKim@users.noreply.github.com>
2024-10-18 22:09:43 +0900

    Title: 두 사람, Time: 188ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241018/두 사람/two-person.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let a = input[0].split(" ")
+let b = input[1].split(" ")
+
+let ageA = a[0], genderA = a[1]
+let ageB = b[0], genderB = b[1]
+
+if (ageA > 19 || ageB > 19 && genderA === "M" || genderB === "M") {
+    console.log(1)
+} else {
+    console.log(0)
+}
