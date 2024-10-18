6836874202eec04510baaa3ef718292eea882aeb
Minsu Kim <70056622+DevMinsuKim@users.noreply.github.com>
2024-10-18 23:32:18 +0900

    Title: 자연수면 출력, Time: 232ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241018/자연수면 출력/output-only-natural-number.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let arr = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(arr[0]);
+let b = Number(arr[1]);
+
+let result = ''
+if (a >= 0) {
+    for(let i = 0; i < b; i++) {
+        result += String(a)
+    }
+    console.log(result);
+} else {
+    console.log(0)
+}
