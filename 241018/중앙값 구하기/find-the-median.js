8d3c4c8c633b8e94cd68a90a4a29132ea1a6f323
Minsu Kim <70056622+DevMinsuKim@users.noreply.github.com>
2024-10-18 22:47:37 +0900

    Title: 중앙값 구하기, Time: 181ms, Memory: 21MB, Status: Passed - CodeTree

--- 241018/중앙값 구하기/find-the-median.js
+++ 241018/중앙값 구하기/find-the-median.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 let arr = fs.readFileSync(0).toString().trim().split(" ");
 
-let sortArr = arr.map(Number).sort()
+let sortArr = arr.map(Number).sort((a,b) => a - b)
 
 console.log(sortArr[1])
