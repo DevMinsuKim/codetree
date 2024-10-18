dbb4869fc1a10536eb6b872be21d307af0d33a9a
Minsu Kim <70056622+DevMinsuKim@users.noreply.github.com>
2024-10-18 21:59:28 +0900

    Title: 좀 더 어려운 수학 점수, Time: 172ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241018/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
+++ 241018/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
@@ -4,10 +4,10 @@ let input = fs.readFileSync(0).toString().trim().split("\n");
 let A = input[0].split(" ")
 let B = input[1].split(" ")
 
-if (A[0] > B[0]) {
+if (Number(A[0]) > Number(B[0])) {
     console.log("A")
-} else if (A[0] === B[0]) {
-    if (A[1] > B[1]) {
+} else if (Number(A[0]) === Number(B[0])) {
+    if (Number(A[1]) > Number(B[1])) {
         console.log("A")
     }
 } else {
