f7316488fc0f603eb9c293d86e3a9764314c29c7
Minsu Kim <70056622+DevMinsuKim@users.noreply.github.com>
2024-10-20 00:20:52 +0900

    Title: 더 긴 문자열, Time: 223ms, Memory: 14MB, Status: Passed - CodeTree

--- 241020/더 긴 문자열/longer-string.js
+++ 241020/더 긴 문자열/longer-string.js
@@ -5,9 +5,9 @@ let a = input[0];
 let b = input[1];
 
 if(a.length > b.length) {
-    console.log(a)
-} else if (a.length < b.legnth) {
-    console.log(b);
+    console.log(a, a.length)
+} else if (a.length < b.length) {
+    console.log(b, b.length);
 } else {
     console.log("same");
 }
